c819f7abd6cd270000e366c0099a9eb35e7e58ee
VG <vladyslav.gnatiuk@gmail.com>
2025-02-06 09:38:26 -0500

    Launch aspnet api

--- src/QueueApi/Program.cs
+++ src/QueueApi/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace QueueApi
 {
@@ -6,7 +10,38 @@ namespace QueueApi
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var host = Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.ConfigureKestrel((context, serverOptions) =>
+                    {
+                        int port = int.Parse(context.Configuration["PORT"] ?? "5000");
+                        serverOptions.ListenAnyIP(port);
+                    });
+                    webBuilder.ConfigureServices(services =>
+                    {
+                        services.AddControllers();
+                    });
+                    webBuilder.Configure((context, app) =>
+                    {
+                        var env = context.HostingEnvironment;
+
+                        if (env.IsDevelopment())
+                        {
+                            app.UseDeveloperExceptionPage();
+                        }
+
+                        app.UseRouting();
+
+                        app.UseEndpoints(endpoints =>
+                        {
+                            endpoints.MapControllers();
+                        });
+                    });
+                })
+                .Build();
+
+            host.Run();
         }
     }
 }
