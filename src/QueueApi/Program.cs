bdb140d455da3f00df0222a2e12c423e92f297a7
VG <vladyslav.gnatiuk@gmail.com>
2025-02-06 01:14:14 -0500

    Create a c# project in dev container

---
+++ src/QueueApi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QueueApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
